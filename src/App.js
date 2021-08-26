68075356508ba36b3b0234903aaf917720a5c5fe
thiago-porto25 <thiago_porto25@hotmail.com>
2021-08-25 23:18:34 -0300

    set up firebase and tested it

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import { Switch, Route } from 'react-router-dom'
 import { globalStyle as GlobalStyle } from './globalStyles'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
 import Cart from './pages/Cart'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
@@ -8,10 +11,24 @@ import Product from './pages/Product'
 import Shop from './pages/Shop'
 import * as ROUTES from './constants/Routes'
 
+firebase.initializeApp({
+  apiKey: 'AIzaSyCS3EUTzby1m51aWdIZfA0BDT_XEGzpxuo',
+  authDomain: 'music-store-9cab8.firebaseapp.com',
+  projectId: 'music-store-9cab8',
+  storageBucket: 'music-store-9cab8.appspot.com',
+  messagingSenderId: '304103599756',
+  appId: '1:304103599756:web:098671fd5d21fa7cf715a9',
+})
+
+const auth = firebase.auth()
+const firestore = firebase.firestore()
+
 function App() {
-  const firebase = window.firebase
-  console.log(firebase)
   const [cart, setCart] = useState([])
+
+  function addUser() {
+    firestore.collection('users').add({ test: 'test' })
+  }
 
   return (
     <>
